1469a47360ff52e836712d2bd8ed933b0229bba9
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-15 11:58:12 +0100

    install morgan

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world!")
-});
+// bring in routes
+const {getPosts} = require("./routes/post")
+
+app.get("/", getPosts);
 
 //server port
 const port = 3000
