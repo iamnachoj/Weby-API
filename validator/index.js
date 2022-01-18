7d276974190e1d20e25c967a7203ff868252794b
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-18 06:44:07 +0100

    user sign up validation and error messaging

--- validator/index.js
+++ validator/index.js
@@ -22,3 +22,31 @@ exports.createPostValidator = (req, res, next) => {
   // proceed to next middleware
   next();
 }
+
+exports.userSignupValidator = (req, res, next) => {
+  //name
+  req.check("name", "Please, add a name").notEmpty();
+  req.check("name", "Please, use between 4 and 20 characters for your username").isLength({
+    min: 4,
+    max: 20
+  });
+  //password
+  req.check("password", "Please, enter a password").notEmpty();
+  req.check("password", "Please, use a minimum of 8 characters for your password").isLength({
+    min: 8,
+    max: 50
+  });
+  req.check("password", "Password must contain at least 1 number").matches(/\d/)
+  //email
+  req.check("email", "Please, add an email").notEmpty();
+  req.check("email", "Please, add a valid email").isEmail();
+  //check for errors
+  const errors = req.validationErrors()
+  //if error show the first one as they happen
+  if(errors){
+    const firstError = errors.map((error) => error.msg)[0]
+    return res.status(400).json({error: firstError})
+  }
+  // proceed to next middleware
+  next();
+}
