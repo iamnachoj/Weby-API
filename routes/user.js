3f4bab7d6ffc25086eff149864d1b583f3fe44a7
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-21 18:55:44 +0100

    add new fixed method to update and delete users. Now the authorized user can only delete or update its own profile

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {userById, allUsers, getUser, updateUser, deleteUser} = require("../controllers/user")
+const {userById, hasAuthorization, allUsers, getUser, updateUser, deleteUser} = require("../controllers/user")
 const {postById} = require("../controllers/post");
 const {requireSignin} = require("../controllers/auth");
 
@@ -12,6 +12,6 @@ router.param("postId", postById);
 
 router.get("/users", allUsers)
 router.get("/users/:userId", requireSignin, getUser)
-router.put("/users/:userId", requireSignin, updateUser)
-router.delete("/users/:userId", requireSignin, deleteUser)
+router.put("/users/:userId", requireSignin, hasAuthorization, updateUser)
+router.delete("/users/:userId", requireSignin, hasAuthorization, deleteUser)
 module.exports = router;
