57a6d727a0b8d68c46f1fd836ba30600f98c6635
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-01-22 15:46:16 +0100

    create a get request to / in order to get de API documentation and end-points

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const express = require("express");
 const morgan = require("morgan"); // tracks info about requests
 const cookieParser = require("cookie-parser");
 const expressValidator = require("express-validator");
+const fs = require("fs");
 const mongoose = require("mongoose");
 const app = express();
 
@@ -24,8 +25,20 @@ mongoose.connection.on("error", err => {
 // bring in routes
 const postRoutes = require("./routes/post");
 const authRoutes = require("./routes/auth");
-const userRoutes = require("./routes/user")
-
+const userRoutes = require("./routes/user");
+const { json } = require("express/lib/response");
+//api-docs
+app.get("/", (req, res) => {
+  fs.readFile("docs/api-docs.json", (err, data) => {
+    if(err){
+      res.status(400).json({
+        error: err
+      })
+    }
+    const docs = JSON.parse(data);
+    res.json(docs)
+  })
+})
 // middleware
 app.use(morgan('dev'))
 app.use(express.urlencoded({extended: true})); 
