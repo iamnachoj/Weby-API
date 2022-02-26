67c85df1b21d86166c16f1823bd3099b0cabbf1e
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2022-02-26 23:17:11 +0100

    set contenttype to right key:mimetype

--- controllers/user.js
+++ controllers/user.js
@@ -87,7 +87,7 @@ exports.updateUser = (req, res, next) => {
 
       if (files.avatar) {
           user.avatar.data = fs.readFileSync(files.avatar.filepath);
-          user.avatar.contentType = files.avatar.type;
+          user.avatar.contentType = files.avatar.mimetype;
       }
 
       user.save((err, result) => {
